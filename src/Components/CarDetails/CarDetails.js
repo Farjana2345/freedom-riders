8bfbd176e30a31e47c444420354c36d0ba35ee3b
Farjana2345 <fa8547830@gmail.com>
2021-03-21 17:23:22 +0100

    freedom riders

--- src/Components/CarDetails/CarDetails.js
+++ src/Components/CarDetails/CarDetails.js
@@ -4,14 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserAlt } from '@fortawesome/free-solid-svg-icons'
 
 const CarDetails = ({singleCar}) => {
-    const{name,img,img1,img2,amount,ticket}=singleCar;
+    const{name,img,amount,ticket}=singleCar;
     return (
         <div className="carDetails-area">
-            <div className="row">
-                <div className="col-md-12">
-                    
-                </div>
-            </div>
             <div className="details-area">
                 <div className="row background-image">
                     <div className="col-md-12">
@@ -19,28 +14,7 @@ const CarDetails = ({singleCar}) => {
                             <img src={img} alt=""/>
                             <h4>{name}</h4>
                             <h4 style={{display:'flex'}}><FontAwesomeIcon icon={faUserAlt} /> {ticket}</h4>
-                            <p>$ {amount}</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="row background-image">
-                    <div className="col-md-12">
-                        <div className="image">
-                            <img src={img1} alt=""/>
-                            <h4>{name}</h4>
-                            <h4 style={{display:'flex'}}><FontAwesomeIcon icon={faUserAlt} /> {ticket}</h4>
-                            <p>$ {amount}</p>
-                        </div>
-                    </div>
-                   
-                </div>
-                <div className="row background-image">
-                    <div className="col-md-12">
-                        <div className="image">
-                            <img src={img2} alt=""/>
-                            <h4>{name}</h4>
-                            <h4 style={{display:'flex'}}><FontAwesomeIcon icon={faUserAlt} /> {ticket}</h4>
-                            <p>$ {amount}</p>
+                            <h4>$ {amount}</h4>
                         </div>
                     </div>
                 </div>
